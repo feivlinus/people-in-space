1e9589b177ad25d7c11f8af144c148135ae6b45d
Felix Schmidt <para.feivl.dox@gmail.com>
2023-10-13 08:57:23 +0200

    Completed additionally challenge

---
+++ script.js
@@ -0,0 +1,24 @@
+console.log("Do i have my sanity?");
+
+const peopleInSpaceSpan = document.querySelector(
+	'[data-js="peopleInSpaceNumber"]'
+);
+
+function setPeopleInSpace(number) {
+	peopleInSpaceSpan.textContent = `${number}`;
+}
+
+async function getPeopleInSpace() {
+	const url = "http://api.open-notify.org/astros.json";
+	const response = await fetch(url);
+
+	const data = await response.json();
+	console.log(data);
+
+	const peopleInSpaceNumber = data.number;
+	return peopleInSpaceNumber;
+}
+
+getPeopleInSpace().then((value) => {
+	setPeopleInSpace(value);
+});
